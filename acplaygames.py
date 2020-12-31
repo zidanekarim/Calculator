9bcbf6c3ec85e64f85164f689d52a4b780531862
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2020-12-31 12:32:43 -0500

    Update acplaygames.py

--- acplaygames.py
+++ acplaygames.py
@@ -1,50 +1,44 @@
 print("Welcome to the Shy Calculator. If you're here, it means you're shy.")
-yeslist = ["Y", "YES", "ye", "y", "Yes", "yes"]
+yeslist = ["Y", "ye", "y", "Yes", "yes"]
 nolist = ["N", "NO", "NAH" "n", "No", "no"]
+def restart():
+    global h
+    cont = input("Would you like to continue?")
+    if cont.lower() in yeslist:
+        print("Ok!")
+        h = 1
+    if cont.lower() in nolist:
+        h = 2
+        print("Alright")
 def calculator():
-    while True:
+    global h
+    h = 1
+    while h == 1:
         try:
             x = float(input("Alright, choose number 1 \n"))
             print("bruh!")
             y = float(input("Alright, choose number 2 \n"))
             print("Alright, let's move on the actual calculator noob!")
-            func = input("What function would you like to operate with? \n +, -, *, or / ? \n" )
+            func = input("What function would you like to operate with? \n +, -, *, or / ? \n")
             if func == "+":
                 answer = (x + y)
                 print(f"Computing... \n x + y = {answer}")
-                cont = input("Would you like to continue?")
-                if cont in yeslist:
-                    print("Ok!")
-                if cont in nolist:
-                    break
+                restart()
             elif func == "-":
                 answer = (x - y)
                 print(f"Computing... \n x + y = {answer}")
-                cont = input("Would you like to continue?")
-                if cont in yeslist:
-                    print("Ok!")
-                if cont in nolist:
-                    break
+                restart()
+
             elif func == "*":
                 answer = (x * y)
                 print(f"Computing... \n x + y = {answer}")
-                cont = input("Would you like to continue?")
-                if cont in yeslist:
-                    print("Ok!")
-                if cont in nolist:
-                    break
+                restart()
+
             elif func == "/":
                 answer = (x / y)
                 print(f"Computing... \n x + y = {answer}")
-                cont = input("Would you like to continue?")
-                if cont in yeslist:
-                    print("Ok!")
-                if cont in nolist:
-                    break
+                restart()
         except:
             print('You did not type in a number')
             calculator()
-
 calculator()
-
-
