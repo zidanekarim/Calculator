42e08a4ff874569248fb26555dc347638b8170ea
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2020-12-31 16:29:53 -0500

    Update acplaygames.py

--- acplaygames.py
+++ acplaygames.py
@@ -1,6 +1,8 @@
-print("Welcome to the Shy Calculator. If you're here, it means you're shy.")
+print("Calculator starting in \n 3...2...1")
 yeslist = ["Y", "ye", "y", "Yes", "yes"]
-nolist = ["N", "NO", "NAH" "n", "No", "no"]
+nolist = ["N", "NO", "NAH", "n", "No", "no"]
+
+
 def restart():
     global h
     cont = input("Would you like to continue?")
@@ -10,35 +12,38 @@ def restart():
     if cont.lower() in nolist:
         h = 2
         print("Alright")
+
+
 def calculator():
     global h
     h = 1
     while h == 1:
         try:
             x = float(input("Alright, choose number 1 \n"))
-            print("bruh!")
             y = float(input("Alright, choose number 2 \n"))
-            print("Alright, let's move on the actual calculator noob!")
+            print("Alright, let's move on the actual calculator!")
             func = input("What function would you like to operate with? \n +, -, *, or / ? \n")
             if func == "+":
                 answer = (x + y)
-                print(f"Computing... \n x + y = {answer}")
+                print("Computing... \n" + str(x) + " + " + str(y) + " = " + str(answer))
                 restart()
             elif func == "-":
                 answer = (x - y)
-                print(f"Computing... \n x + y = {answer}")
+                print("Computing... \n" + str(x) + " - " + str(y) + " = " + str(answer))
                 restart()
 
             elif func == "*":
                 answer = (x * y)
-                print(f"Computing... \n x + y = {answer}")
+                print("Computing... \n" + str(x) + " * " + str(y) + " = " + str(answer))
                 restart()
 
             elif func == "/":
                 answer = (x / y)
-                print(f"Computing... \n x + y = {answer}")
+                print("Computing... \n" + str(x) + " / " + str(y) + " = " + str(answer))
                 restart()
-        except:
+        except():
             print('You did not type in a number')
             calculator()
+
+
 calculator()
